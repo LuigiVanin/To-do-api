651f9330e3c2f91a196dc176c6887282b5c5d4af
LuigiVanin <luisfvaninmartins@gmail.com>
2021-06-14 15:32:00 -0300

    add ini file -update

--- config.py
+++ config.py
@@ -3,12 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+from configparser import ConfigParser
 
+
+config = ConfigParser()
+config.read("confi.ini")
 # SQL_URL="mysql://name:password@host:port/dbname"
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY = config["SECURITY"]["key"] #"09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = config["SECURITY"]["algorithm"]
 EXPIRE_TIME = 30
-SQL_URL = "sqlite:///data.db"
+SQL_URL = config["DATABASE"]["url"]
 
 engine = create_engine(SQL_URL,
                        connect_args={"check_same_thread": False})
